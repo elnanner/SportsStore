98ad3b9db1af36c959521f275140c788ab413b2a
Luciano Matsuo <matsuoluciano@gmail.com>
2018-05-20 21:10:08 -0300

    agregado archivos js

---
+++ SportsStore/SportsStore/Scripts/App/storeCommonController.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module("App", []);
+app.controller("commonController", function ($scope) {
+    var authenticateUrl = "/authenticate";
+
+    $scope.testAuth = function () {
+        username = "Admin";
+        password = "secret";
+        this.authenticate();
+    }
+    $scope.authenticate = function (successCallback) {
+        sendRequest(
+            authenticateUrl,
+            "POST",
+            { "grant_type": "password", username: username, password: password },
+            function (data) {
+                model.authenticated = true;
+                setAjaxHeaders({ Authorization: "bearer " + data.access_token });
+                if (successCallback) {
+                    successCallback();
+                }
+            });
+    };
+});
